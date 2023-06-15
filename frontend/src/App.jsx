b179e97f2f1a3c674105ba3a675852c0d6127d47
30Xp107 <jhanbrent0630@gmail.com>
2023-06-15 13:47:11 +0800

    FrontEnd

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Header from './components/Header.jsx'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <ToastContainer />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
+    </>
+  )
+}
+
+export default App;
+
+// I had to restart my server for some reason. So if you run into an issue, try that first.
